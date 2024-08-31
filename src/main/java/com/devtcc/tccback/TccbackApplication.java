49f2b256134bc9de989c3383908b6b4734180475
João L. V. Parrilha <jl.parrilha1999@gmail.com>
2024-08-31 10:53:10 -0300

    Criação do Projeto

---
+++ src/main/java/com/devtcc/tccback/TccbackApplication.java
@@ -0,0 +1,13 @@
+package com.devtcc.tccback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devtcc/tccback/TccbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtcc.tccback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
